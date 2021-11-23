a27086cda3a63fbc8d41e171af076939c78bab48
yuizho <yuizho3@gmail.com>
2021-11-23 11:09:09 +0900

    add Fill.asm

--- 4_machine_lang/fill/Fill.asm
+++ 4_machine_lang/fill/Fill.asm
@@ -12,3 +12,59 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+    @0 // flag typed or untyped
+    M=0
+    @1 // screen index
+    M=-1
+(LOOP)
+    // reset screen when screen is fille
+    @1
+    D=M
+    @KBD // last index of screen
+    D=D-A
+    @RESET_SECREEN_INDEX
+    D;JEQ
+    // check if keyboead is typed or not
+    @KBD
+    D=M
+    @TYPED
+    D;JNE
+    @UNTYPED
+    D;JEQ
+(TYPED)
+    // show black to screen
+    @1
+    D=M+1
+    M=D
+    @SCREEN
+    A=D+A
+    M=-1
+    // reset screen index if needed
+    @0
+    D=M
+    M=-1
+    @RESET_SECREEN_INDEX
+    D;JEQ
+    @LOOP
+    0;JMP
+(UNTYPED)
+    // show black to screen
+    @1
+    D=M+1
+    M=D
+    @SCREEN
+    A=D+A
+    M=0
+    // reset sreen index if needed
+    @0
+    D=M
+    M=0
+    @RESET_SECREEN_INDEX
+    D;JNE
+    @LOOP
+    0;JMP
+(RESET_SECREEN_INDEX)
+    @1
+    M=-1
+    @LOOP
+    0;JMP
