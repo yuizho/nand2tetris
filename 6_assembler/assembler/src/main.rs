4ac50e2c39b28d39e89bf180893a5cc4e6daa770
yuizho <yuizho3@gmail.com>
2021-12-05 17:26:55 +0900

    implement non symbol assembler

--- 6_assembler/assembler/src/main.rs
+++ 6_assembler/assembler/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::{BufWriter, Write};
 mod parser;
 
 fn main() {
@@ -6,17 +8,30 @@ fn main() {
     let filename = get_filename(&args);
 
     let mut parser = parser::Parser::create(filename);
+    let compiled_filename = filename.replace(".asm", ".hack");
+    let mut f =
+        BufWriter::new(File::create(compiled_filename).expect("failed to create hack file."));
     while parser.has_more_commands() {
+        // parse nimonic to commant_type obj
         let command_type = parser.command_type();
-        println!("{:?}", command_type);
-
+        // convert command_type obj to binary
+        let code = parser::convert_code(command_type);
+        if let Some(code) = code {
+            let binary_instruction = format!("{:016b}", code) + "\n";
+            f.write(binary_instruction.as_bytes())
+                .expect("failed to write hack file");
+        }
         parser.advance();
     }
+    f.flush().expect("failed to write hack file");
 }
 
 fn get_filename(args: &Vec<String>) -> &String {
     if args.len() < 2 {
         panic!("this command needs at least one argument")
+    }
+    if !args[1].ends_with(".asm") {
+        panic!("the file is not expected assembly file.")
     }
     &args[1]
 }
--- 6_assembler/assembler/src/parser.rs
+++ 6_assembler/assembler/src/parser.rs
@@ -64,3 +64,77 @@ impl CommandType {
         }
     }
 }
+
+pub fn convert_code(command_type: CommandType) -> Option<i32> {
+    match command_type {
+        CommandType::A(symbol) => Some(symbol as i32),
+        CommandType::C(dest, cmp, jmp) => {
+            Some(0b1110000000000000 + convert_comp(cmp) + convert_dest(&dest) + convert_jump(&jmp))
+        }
+        // TODO: L
+        _ => None,
+    }
+}
+
+fn convert_comp(cmp: String) -> i32 {
+    let trimed_cmp = cmp.replace(" ", "");
+    match trimed_cmp.as_str() {
+        "0" => 0b0101010000000,
+        "1" => 0b0111111000000,
+        "-1" => 0b0111010000000,
+        "D" => 0b0001100000000,
+        "A" => 0b0110000000000,
+        "!D" => 0b0001101000000,
+        "!A" => 0b0110001000000,
+        "-D" => 0b0001111000000,
+        "-A" => 0b0110011000000,
+        "D+1" => 0b0011111000000,
+        "A+1" => 0b0110111000000,
+        "D-1" => 0b0001110000000,
+        "A-1" => 0b0110010000000,
+        "D+A" => 0b0000010000000,
+        "D-A" => 0b0010011000000,
+        "A-D" => 0b0000111000000,
+        "D&A" => 0b0000000000000,
+        "D|A" => 0b0010101000000,
+        "M" => 0b1110000000000,
+        "!M" => 0b1110001000000,
+        "-M" => 0b1110011000000,
+        "M+1" => 0b1110111000000,
+        "M-1" => 0b1110010000000,
+        "D+M" => 0b1000010000000,
+        "D-M" => 0b1010011000000,
+        "M-D" => 0b1000111000000,
+        "D&M" => 0b1000000000000,
+        "D|M" => 0b1010101000000,
+        _ => panic!("unexpected cmp code is comming: {}", cmp),
+    }
+}
+
+fn convert_dest(dest: &Option<String>) -> i32 {
+    match dest.as_deref() {
+        Some("M") => 0b001000,
+        Some("D") => 0b010000,
+        Some("MD") => 0b011000,
+        Some("A") => 0b100000,
+        Some("AM") => 0b101000,
+        Some("AD") => 0b110000,
+        Some("AMD") => 0b111000,
+        None => 0b000000,
+        Some(a) => panic!("unexpected dest code is comming: {}", a),
+    }
+}
+
+fn convert_jump(jmp: &Option<String>) -> i32 {
+    match jmp.as_deref() {
+        Some("JGT") => 0b001,
+        Some("JEQ") => 0b010,
+        Some("JGE") => 0b011,
+        Some("JLT") => 0b100,
+        Some("JNE") => 0b101,
+        Some("JLE") => 0b110,
+        Some("JMP") => 0b111,
+        None => 0b000,
+        Some(a) => panic!("unexpected jump code is comming: {}", a),
+    }
+}
