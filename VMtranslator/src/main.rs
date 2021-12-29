d06a2044989b0889d8bcff4502cddd19d64c6f42
yuizho <yuizho3@gmail.com>
2021-12-29 16:24:06 +0900

    modify VMtranslator
    implement directory reading

--- VMtranslator/src/main.rs
+++ VMtranslator/src/main.rs
@@ -1,38 +1,63 @@
 use std::env;
+use std::ffi::OsStr;
 use std::fs::File;
 use std::io::BufWriter;
+use std::path::{Path, PathBuf};
 
 mod translator;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = get_filename(&args);
+    let filenames = get_filenames(&args);
 
-    let f = File::open(filename).expect("file not found");
-    let mut parser = translator::Parser::create(f);
+    for filename in filenames {
+        println!("converts {:?} to .asm file", filename);
 
-    let mut buf_writer = BufWriter::new(
-        File::create(filename.replace(".vm", ".asm").as_str()).expect("failed to create asm file."),
-    );
-    let mut code_writer = translator::CodeWriter::create(&mut buf_writer);
+        let f = File::open(&filename).expect("file not found");
+        let mut parser = translator::Parser::create(f);
 
-    while parser.has_more_commands() {
-        parser.advance();
-        let command_type = parser.command_type();
-        println!("{:?}", command_type);
-        code_writer.write_command(&command_type);
+        let mut buf_writer = BufWriter::new(
+            File::create(&filename.with_extension("asm")).expect("failed to create asm file."),
+        );
+        let mut code_writer = translator::CodeWriter::create(&mut buf_writer);
+
+        while parser.has_more_commands() {
+            parser.advance();
+            let command_type = parser.command_type();
+            println!("{:?}", command_type);
+            code_writer.write_command(&command_type);
+        }
+
+        code_writer.flush()
     }
-
-    code_writer.flush()
 }
 
-fn get_filename(args: &Vec<String>) -> &String {
+fn get_filenames(args: &Vec<String>) -> Vec<PathBuf> {
     if args.len() < 2 {
         panic!("this command needs at least one argument")
     }
-    // TODO: needs to read directory too
-    if !args[1].ends_with(".vm") {
-        panic!("the file is not expected assembly file.")
+
+    let path = Path::new(&args[1]);
+    if path.is_dir() {
+        // treats as directory
+        let dir = std::fs::read_dir(&args[1]).expect("failed to read dir");
+        let filenames = dir
+            .into_iter()
+            .map(|f| f.expect("failed to read files in dir").path())
+            .filter(|path_buf| path_buf.as_path().extension().unwrap_or(OsStr::new("")) == "vm")
+            .collect::<Vec<PathBuf>>();
+        if filenames.is_empty() {
+            panic!("there is no .vm file.");
+        }
+        return filenames;
     }
-    &args[1]
+
+    // treats as file
+    match path.extension() {
+        Some(extension) => match extension.to_str().unwrap_or("") {
+            "vm" => vec![path.to_path_buf()],
+            unexpected_extension => panic!("unexpected file extension: {}.", unexpected_extension),
+        },
+        None => panic!("the specified file doesn't have extension."),
+    }
 }
