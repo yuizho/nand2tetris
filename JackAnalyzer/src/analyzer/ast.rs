2bd916e1cc025247f2f766b516c3f152f52c10de
yuizho <yuizho3@gmail.com>
2022-01-11 13:36:27 +0900

    modify ast.rs to add to_xml methods

--- JackAnalyzer/src/analyzer/ast.rs
+++ JackAnalyzer/src/analyzer/ast.rs
@@ -20,6 +20,18 @@ impl Node {
             Self::Expression(expression) => expression.token_literal(),
         }
     }
+
+    pub fn to_xml(&self) -> String {
+        match self {
+            Self::Program(staements) => staements
+                .iter()
+                .map(|s| s.to_xml())
+                .collect::<Vec<_>>()
+                .join("\n"),
+            Self::Statement(statement) => statement.to_xml(),
+            Self::Expression(expression) => expression.to_xml(),
+        }
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -33,6 +45,24 @@ impl Statement {
         match self {
             Self::LetStatement(token, _, _) => token.get_literal(),
             Self::ReturnStatement(token, _) => token.get_literal(),
+        }
+    }
+    pub fn to_xml(&self) -> String {
+        match self {
+            Self::LetStatement(_, identifier, expression) => {
+                format!(
+                    "<letStatement>\n  {}\n  {}\n  {}\n  {}\n  {}\n</letStatement>",
+                    "<keyword> let </keyword>",
+                    identifier.to_xml(),
+                    "<symbol> = </symbol>",
+                    format!(
+                        "<expression>\n    <term>\n      {}\n    </term>\n  </expression>",
+                        expression.to_xml()
+                    ),
+                    "<symbol> ; </symbol>"
+                )
+            }
+            Self::ReturnStatement(_, _) => "return".to_string(),
         }
     }
 }
@@ -49,5 +79,44 @@ impl Expression {
             Self::Identifier(token) => token.get_literal(),
             Self::Dummy => "".to_string(),
         }
+    }
+
+    pub fn to_xml(&self) -> String {
+        match self {
+            Self::Identifier(token) => {
+                format!("<identifier> {} </identifier>", token.get_literal())
+            }
+            Self::Dummy => "dummy".to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::analyzer::ast::*;
+    use crate::analyzer::token::*;
+
+    #[test]
+    fn to_xml() {
+        let program = Node::Program(vec![Statement::LetStatement(
+            TokenType::KEYWORD(Keyword::LET),
+            Expression::Identifier(TokenType::IDNETIFIER("myVar".to_string())),
+            Expression::Identifier(TokenType::IDNETIFIER("anotherVar".to_string())),
+        )]);
+
+        assert_eq!(
+            program.to_xml(),
+            "<letStatement>
+  <keyword> let </keyword>
+  <identifier> myVar </identifier>
+  <symbol> = </symbol>
+  <expression>
+    <term>
+      <identifier> anotherVar </identifier>
+    </term>
+  </expression>
+  <symbol> ; </symbol>
+</letStatement>"
+        )
     }
 }
