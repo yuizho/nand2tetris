b837149da66bfbaf0470ff5cfe7d2be49a1d81b9
yuizho <yuizho3@gmail.com>
2022-01-22 14:42:03 +0900

    modify compiler.rs

--- JackAnalyzer/src/analyzer/compiler.rs
+++ JackAnalyzer/src/analyzer/compiler.rs
@@ -8,9 +8,7 @@ pub struct CompilationEngine<'a, T: Write> {
 
 impl<'a, T: Write> CompilationEngine<'a, T> {
     pub fn new(buf_writer: &'a mut BufWriter<T>) -> Self {
-        CompilationEngine {
-            buf_writer: buf_writer,
-        }
+        CompilationEngine { buf_writer }
     }
 
     pub fn compile(&mut self, program_ast: Program) {
