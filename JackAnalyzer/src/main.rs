696475de49aba2a3b8801aeb7c8d8b6baece9870
yuizho <yuizho3@gmail.com>
2022-03-05 11:20:05 +0900

    modify JackAnalyzer to add xml mode

--- JackAnalyzer/src/main.rs
+++ JackAnalyzer/src/main.rs
@@ -8,13 +8,16 @@ mod analyzer;
 use analyzer::parser::Parser;
 use analyzer::tokenizer::JackTokenizer;
 use analyzer::vm::VmWriter;
+use analyzer::xml::XmlWriter;
+use std::str::FromStr;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let file_names = get_source_file_paths(&args)?;
+    let output_mode = get_outpout_option(&args)?;
 
     for file_name in file_names {
-        let new_file_name = file_name.with_extension(OsStr::new("vm"));
+        let new_file_name = file_name.with_extension(OsStr::new(output_mode.extension()));
         let mut buf_writer = BufWriter::new(File::create(new_file_name)?);
         let f = File::open(&file_name)?;
 
@@ -24,13 +27,60 @@ fn main() -> Result<()> {
             // filename is same as class name
             file_name.file_stem().unwrap().to_str().unwrap().to_string(),
         );
-        let vm = parser.parse_program()?.to_vm();
 
-        buf_writer.write_vm(&vm)?;
+        let program_ast = parser.parse_program()?;
+        match output_mode {
+            OutputMode::Vm => buf_writer.write_vm(&program_ast.to_vm())?,
+            OutputMode::Xml => buf_writer.write_xml(&program_ast.to_xml())?,
+        }
+
         buf_writer.flush()?;
     }
 
     Ok(())
+}
+
+enum OutputMode {
+    Vm,
+    Xml,
+}
+
+impl OutputMode {
+    fn extension(&self) -> &str {
+        use OutputMode::*;
+        match self {
+            Vm => "vm",
+            Xml => "xml",
+        }
+    }
+}
+
+impl FromStr for OutputMode {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<OutputMode, Self::Err> {
+        use OutputMode::*;
+        match input {
+            "vm" => Ok(Vm),
+            "xml" => Ok(Xml),
+            _ => Err(()),
+        }
+    }
+}
+
+fn get_outpout_option(args: &[String]) -> Result<OutputMode> {
+    use OutputMode::*;
+    if args.len() < 3 {
+        return Ok(Vm);
+    }
+
+    let output_option = &args[2];
+    match OutputMode::from_str(output_option) {
+        Ok(option) => Ok(option),
+        Err(_) => Err(anyhow!(
+            "unexpected output option is passed. please set 'vm'(default) or 'xml'."
+        )),
+    }
 }
 
 fn get_source_file_paths(args: &[String]) -> Result<Vec<PathBuf>> {
